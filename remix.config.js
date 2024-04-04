03dc663a2e934cb21843f75363aecf8d2a44763d
Artem Zakharchenko <kettanaito@gmail.com>
2024-04-04 14:06:39 +0200

    Initial commit from create-remix

---
+++ remix.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+  serverModuleFormat: "esm",
+  tailwind: true,
+  postcss: true,
+};
