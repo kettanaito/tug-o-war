316948dd8709c0624250699fd4699258eaf9cba0
Artem Zakharchenko <kettanaito@gmail.com>
2024-04-09 10:30:11 -0600

    improve landscape component layout

--- app/components/orientation.tsx
+++ app/components/orientation.tsx
@@ -3,17 +3,29 @@ export function Orientation() {
     <aside
       style={{
         position: 'fixed',
-        padding: '1rem',
         top: 0,
         left: 0,
         height: '100%',
         width: '100%',
         backgroundColor: 'white',
         zIndex: '10',
+        textAlign: 'center',
       }}
     >
-      <h2>Horizontal mode</h2>
-      <p>Please turn your device to a landscape mode. Thank you!</p>
+      <div style={{ padding: '5vw' }}>
+        <img
+          src="/landscape-mode.svg"
+          style={{
+            display: 'flex',
+            width: '32vw',
+            maxWidth: '80%',
+            margin: 'auto',
+            opacity: 0.5,
+          }}
+        />
+        <h2>Landscape mode</h2>
+        <p>Please turn your device to a landscape mode. Thank you!</p>
+      </div>
     </aside>
   )
 }
