359ec1e75d784ef36115fb6862668c4d8ba3a6c8
Artem Zakharchenko <kettanaito@gmail.com>
2024-04-09 09:11:45 -0600

    add layout for game-end screen

--- app/components/game-end.tsx
+++ app/components/game-end.tsx
@@ -6,13 +6,38 @@ export function GameEnd({
   winningTeam: GameTeam | undefined
 }) {
   return (
-    <div>
-      <h2>The game is over!</h2>
-      {winningTeam ? (
-        <p>{winningTeam} has won!</p>
-      ) : (
-        <p>Looks like it's a draw!</p>
-      )}
+    <div
+      style={{
+        background: 'url(/assets/board.png) no-repeat top center',
+        backgroundSize: 'cover',
+        width: '50vw',
+        maxWidth: '100%',
+        aspectRatio: '2.85 / 1',
+        margin: '16.5vw auto',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div
+        style={{
+          marginTop: '-3vw',
+          fontSize: '3vw',
+          textAlign: 'center',
+          textShadow: '0 0.25vw 0 rgba(255,255,255,0.25)',
+        }}
+      >
+        <h2 style={{ fontSize: '5vw', margin: '0 0 0.5vw' }}>
+          That's the game!
+        </h2>
+        <p style={{ margin: 0 }}>
+          {winningTeam ? (
+            <>{winningTeam} has won!</>
+          ) : (
+            <>Looks like it's a DRAW!</>
+          )}
+        </p>
+      </div>
     </div>
   )
 }
